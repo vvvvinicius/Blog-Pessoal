f83e9d2a7fbb59dfcdc6934ae56a10e836c8e4a7
Your Name <you@example.com>
2022-02-14 13:57:18 -0300

    Adicionando todos os arquivos do blog pessoal + react

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+    <>
+        <h1 className='titulo'>HOME</h1>
+        <img src="https://opendoodles.s3-us-west-1.amazonaws.com/bikini.png" alt="ilustração" className='img'/>
+    </>
+    );
+}
+
+export default Home;
